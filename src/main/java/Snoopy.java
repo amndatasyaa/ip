55dba62d9c3b85cdd57e377b8ada1cb31bab60b6
amndatasyaa <amandatasyadedi@gmail.com>
2026-08-19 22:11:17 +0800

    Complete Level-0

---
+++ src/main/java/Snoopy.java
@@ -0,0 +1,22 @@
+/**
+ * Starts the Snoopy chatbot application.
+ */
+public class Snoopy {
+    public static void main(String[] args) {
+        String divider = "____________________________________________________________";
+        String banner = "  ____\n"
+                + " / ___| _ __   ___   ___  _ __  _   _\n"
+                + " \\___ \\| '_ \\ / _ \\ / _ \\| '_ \\| | | |\n"
+                + "  ___) | | | | (_) | (_) | |_) | |_| |\n"
+                + " |____/|_| |_|\\___/ \\___/| .__/ \\__, |\n"
+                + "                            |_|    |___/";
+
+        System.out.println(divider);
+        System.out.println(banner);
+        System.out.println("Hi! I'm Snoopy.");
+        System.out.println("How can I help you today?");
+        System.out.println(divider);
+        System.out.println("Bye! See you again soon.");
+        System.out.println(divider);
+    }
+}
