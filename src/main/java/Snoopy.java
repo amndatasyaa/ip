a7a91e549ca1902a806ce58db503c1634df51797
amndatasyaa <amandatasyadedi@gmail.com>
2026-08-19 22:39:16 +0800

    Add support for marking tasks as done
    Track completion separately from task descriptions so list output can show each task's current status without introducing new classes.

--- src/main/java/Snoopy.java
+++ src/main/java/Snoopy.java
@@ -25,6 +25,7 @@ public class Snoopy {
         System.out.println(divider);
 
         String[] tasks = new String[100];
+        boolean[] isDone = new boolean[100];
         int taskCount = 0;
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
@@ -37,9 +38,17 @@ public class Snoopy {
             }
 
             if (command.equals("list")) {
+                System.out.println(" Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    String status = isDone[i] ? "X" : " ";
+                    System.out.println(" " + (i + 1) + ".[" + status + "] " + tasks[i]);
                 }
+            } else if (command.startsWith("mark ")) {
+                int taskNumber = Integer.parseInt(command.substring(5));
+                int taskIndex = taskNumber - 1;
+                isDone[taskIndex] = true;
+                System.out.println(" Nice! I've marked this task as done:");
+                System.out.println("   [X] " + tasks[taskIndex]);
             } else {
                 tasks[taskCount] = command;
                 taskCount++;
