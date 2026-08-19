3abfadbe68b086f70c1fc7e25856029cd128a7af
amndatasyaa <amandatasyadedi@gmail.com>
2026-08-19 22:49:35 +0800

    Complete Level-3

---
+++ src/main/java/Task.java
@@ -0,0 +1,50 @@
+/**
+ * Represents a task and whether it has been completed.
+ */
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    /**
+     * Creates an incomplete task with the given description.
+     *
+     * @param description text describing the task
+     */
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    /**
+     * Marks this task as completed.
+     */
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    /**
+     * Marks this task as not completed.
+     */
+    public void markAsNotDone() {
+        isDone = false;
+    }
+
+    /**
+     * Returns the symbol used to display this task's completion state.
+     *
+     * @return {@code X} when completed, or a space otherwise
+     */
+    public String getStatusIcon() {
+        return isDone ? "X" : " ";
+    }
+
+    /**
+     * Returns this task in the format shown by the chatbot.
+     *
+     * @return the status icon followed by the task description
+     */
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
+}
